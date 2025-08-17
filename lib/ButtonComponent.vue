96a933c2280594cc85bf1b4c233dfcffbb14ae50
Ing. Ebu Celik <ebucelik1@hotmail.com>
2025-08-17 13:11:09 +0200

    update

--- lib/ButtonComponent.vue
+++ lib/ButtonComponent.vue
@@ -2,14 +2,13 @@
 
 const props = defineProps({
     text: String,
-    link: String,
-    isDownloadable: Boolean
+    link: String
 })
 
 </script>
 
 <template>
-    <a :href="props.link" class="decoration-0" :download="isDownloadable">
+    <a :href="props.link" class="decoration-0">
         <div
             class="font-bold text-center place-content-center p-3 border-1 border-gray-400 bg-white/10 backdrop-blur-xs rounded-md hover:border-1 hover:text-white hover:bg-blue-normal transition delay-75 duration-200 ease-in cursor-pointer">
             <h4 class="text-sm lg:text-2xl">{{ props.text }}</h4>
