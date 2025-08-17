9631249c9a06e45faae7f3d456d96418188033fb
Ing. Ebu Celik <ebucelik1@hotmail.com>
2025-08-17 13:30:30 +0200

    update

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,7 @@
 import HeaderButtonComponent from './HeaderButtonComponent.vue'
 import ButtonComponent from './ButtonComponent.vue'
 import VideoWithTextComponent from './VideoWithTextComponent.vue'
+import ImageWithTextComponent from './ImageWithTextComponent.vue'
 import FooterComponent from './FooterComponent.vue'
 
-export { HeaderButtonComponent, ButtonComponent, VideoWithTextComponent, FooterComponent }
+export { HeaderButtonComponent, ButtonComponent, VideoWithTextComponent, ImageWithTextComponent, FooterComponent }
